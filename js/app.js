50055f5d2d752179dca657719f54f8fa0a680aa9
avrilquach <family.happy89@gmail.com>
2021-07-30 07:29:30 -0600

    up

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
 // fullpage 
-new fullpage('#fullpage', {
-  navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
-  css3: true,
-  scrollingSpeed: 1000,
+$('#fullpage').fullpage({
+  sectionSelector: '.vertical-scrolling',
   navigation: false,
-  slidesNavigation: true,
-  responsiveHeight: 330,
-  controlArrows: false
-});
+  controlArrows: false  
+}); 
 $('.owl-carousel').owlCarousel({
   loop:true,
   autoplay: true,
