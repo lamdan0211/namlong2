f3c9253a69f6e87c5c772da34e833c4b738be7fe
avrilquach <family.happy89@gmail.com>
2021-07-30 20:42:32 -0600

    up

--- js/app.js
+++ js/app.js
@@ -7,12 +7,17 @@
       // first animation
       if( index == 1 ) { 
         $('#section2 .img').addClass('animate__fadeInLeft animate__delay-1s');
-        $('#section2 .ctleft').addClass('animate__fadeInRight animate__delay-2s');
+        $('#section2 .ctleft').addClass('animate__fadeInRight animate__delay-1s');
       }
       if( index == 2 ) { 
         $('#section3 .img').addClass('animate__fadeInRight animate__delay-1s');
-        $('#section3 .ctright').addClass('animate__fadeInLeft animate__delay-2s');
+        $('#section3 .ctright').addClass('animate__fadeInLeft animate__delay-1s');
       }
+      if( index == 3 ) { 
+        $('#section4 .text h2').addClass('animate__bounceInUp animate__delay-1s');
+        $('#section4 .text h3').addClass('animate__bounceInUp animate__delay-2s');
+      }
+      
     }
   });
 })(jQuery);
