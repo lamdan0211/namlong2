6c8df51c24326b34455fc7ea8c8a0b2500caab00
avrilquach <family.happy89@gmail.com>
2021-07-30 21:55:48 -0600

    up

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@
   $('#fullpage').fullpage({
     navigation: false,
     onLeave: function(index, nextIndex, direction) {
-      // first animation
       if( index == 1 ) { 
         $('#section2 .img').addClass('animate__fadeInLeft animate__delay-1s');
         $('#section2 .ctleft').addClass('animate__fadeInRight animate__delay-1s');
