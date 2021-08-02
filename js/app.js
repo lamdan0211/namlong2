722d6bed8facac9580c72363008fd38fc085f8db
avrilquach <family.happy89@gmail.com>
2021-08-02 08:30:36 -0600

    up

--- js/app.js
+++ js/app.js
@@ -53,16 +53,6 @@
           $("#section8 ._left").addClass("go");
         }
       }
-      if($('body').hasClass('intro'))
-      {
-        var section2 = $("#intro1").position().top - top;
-        if(x > section2){
-          $("#intro1 ._right").addClass("go");
-          $("#intro1 ._left").addClass("go");
-        }
-      }
-      
-      
     });
   }
   $("#section6 .tab li").on('click',function(){
