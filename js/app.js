21017247930512f8979869a247a8a4fd17424914
avrilquach <family.happy89@gmail.com>
2021-08-01 19:46:57 -0600

    intro

--- js/app.js
+++ js/app.js
@@ -9,78 +9,94 @@
     $("#section4").css("height", height);
     $(window).scroll(function() {
       var x = $(this).scrollTop();
-      var section2 = $("#section2").position().top - 150;
-      var section3 = $("#section3").position().top - 150;
-      var section4 = $("#section4").position().top - 150;
-      var section5 = $("#section5").position().top - 150;
-      var section6 = $("#section6").position().top - 150;
-      var section7 = $("#section7").position().top - 150;
-      var section8 = $("#section8").position().top - 150;
-      if(x > section2){
-        $("#header").addClass("active");
-        $("#section2 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section2 ._left").addClass("animate__fadeInLeft delay-250 ");
+      if($('body').hasClass('home')){
+        var section2 = $("#section2").position().top - 150;
+        var section3 = $("#section3").position().top - 150;
+        var section4 = $("#section4").position().top - 150;
+        var section5 = $("#section5").position().top - 150;
+        var section6 = $("#section6").position().top - 150;
+        var section7 = $("#section7").position().top - 150;
+        var section8 = $("#section8").position().top - 150;
+        if(x > section2){
+          $("#header").addClass("active");
+          $("#section2 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section2 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#header").removeClass("active");
+          $("#section2 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section2 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
+        if(x > section3){
+          $("#section3 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section3 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#section3 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section3 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
+        if(x > section4){
+          $("#section4 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section4 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#section4 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section4 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
+        if(x > section5){
+          $("#section5 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section5 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#section5 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section5 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
+        if(x > section6){
+          $("#section6 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section6 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else 
+        {
+          $("#section6 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section6 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
+        if(x > section7){
+          $("#section7 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section7 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#section7 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section7 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
+        if(x > section8){
+          $("#section8 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#section8 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#section8 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#section8 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
       }
-      else
+      if($('body').hasClass('intro'))
       {
-        $("#header").removeClass("active");
-        $("#section2 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section2 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        var section2 = $("#intro1").position().top - 150;
+        if(x > section2){
+          $("#intro1 ._right").addClass("animate__fadeInRight delay-250 ");
+          $("#intro1 ._left").addClass("animate__fadeInLeft delay-250 ");
+        }
+        else
+        {
+          $("#intro1 ._right").removeClass("animate__fadeInRight delay-250 ");
+          $("#intro1 ._left").removeClass("animate__fadeInLeft delay-250 ");
+        }
       }
-      if(x > section3){
-        $("#section3 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section3 ._left").addClass("animate__fadeInLeft delay-250 ");
-      }
-      else
-      {
-        $("#section3 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section3 ._left").removeClass("animate__fadeInLeft delay-250 ");
-      }
-      if(x > section4){
-        $("#section4 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section4 ._left").addClass("animate__fadeInLeft delay-250 ");
-      }
-      else
-      {
-        $("#section4 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section4 ._left").removeClass("animate__fadeInLeft delay-250 ");
-      }
-      if(x > section5){
-        $("#section5 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section5 ._left").addClass("animate__fadeInLeft delay-250 ");
-      }
-      else
-      {
-        $("#section5 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section5 ._left").removeClass("animate__fadeInLeft delay-250 ");
-      }
-      if(x > section6){
-        $("#section6 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section6 ._left").addClass("animate__fadeInLeft delay-250 ");
-      }
-      else 
-      {
-        $("#section6 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section6 ._left").removeClass("animate__fadeInLeft delay-250 ");
-      }
-      if(x > section7){
-        $("#section7 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section7 ._left").addClass("animate__fadeInLeft delay-250 ");
-      }
-      else
-      {
-        $("#section7 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section7 ._left").removeClass("animate__fadeInLeft delay-250 ");
-      }
-      if(x > section8){
-        $("#section8 ._right").addClass("animate__fadeInRight delay-250 ");
-        $("#section8 ._left").addClass("animate__fadeInLeft delay-250 ");
-      }
-      else
-      {
-        $("#section8 ._right").removeClass("animate__fadeInRight delay-250 ");
-        $("#section8 ._left").removeClass("animate__fadeInLeft delay-250 ");
-      }
+      
       
     });
   }
