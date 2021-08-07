7788e39a9cee13abad91f1e735afbe19a0d11849
lamdan0211 <57250382+lamdan0211@users.noreply.github.com>
2021-08-07 23:07:10 +0700

    up

--- js/app.js
+++ js/app.js
@@ -8,6 +8,17 @@
       loop: true,
       nav: true,
       autoplay: true,
+      responsive: {
+			0: {
+				items: 1
+			},
+			1000: {
+				items: 3
+			},
+			1400: {
+				items: 4
+			}
+		}
   });
   'use strict';
   // initialize fullPage
